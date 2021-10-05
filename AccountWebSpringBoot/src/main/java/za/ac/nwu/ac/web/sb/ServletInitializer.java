ee1164c007453d4b5085a4b218c01c765be8658f
S3kh-met <lmphahlele@protonmail.com>
2021-10-06 01:11:06 +0200

    Added project files with swagger ui fully functional

---
+++ AccountWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package za.ac.nwu.ac.web.sb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AccountWebSpringBootApplication.class);
+	}
+
+}
---
+++ AccountWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/SwaggerController.java
@@ -0,0 +1,33 @@
+package za.ac.nwu.ac.web.sb.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import za.ac.nwu.ac.domain.service.GeneralResponse;
+
+@RestController
+public class SwaggerController
+{
+    @RequestMapping(value = "/hello")
+    public String Hello(){
+        return "Hello world";
+    }
+
+    @GetMapping("/ping")
+    @ApiOperation(value = "Echo the Ping.", notes = "This echo the ping back to the client")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The Ping was received and echoed", response = GeneralResponse.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+            @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
+    public ResponseEntity<String> ping(
+            @RequestParam(value = "value that will be echoed", defaultValue = "pong") String echo) {
+            return new ResponseEntity<>(echo, HttpStatus.OK);
+    }
+}
